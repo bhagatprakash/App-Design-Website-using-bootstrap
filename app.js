d347512adf9cfe3d9e54508bc17594bc144f7358
prakash bhagat <bhagatprakash1712@gmail.comm>
2023-10-10 18:49:25 +0530

    update

---
+++ app.js
@@ -0,0 +1,52 @@
+// scroll
+function navMenu() {
+  let navBar = document.querySelector(".navbar-sticky");
+  let scrollTopButton = document.querySelector("#scrollUp");
+  window.onscroll = function () {
+    var scroll = document.documentElement.scrollTop;
+    if (scroll >= 120) {
+      navBar.classList.add("navbar-sticky-moved-up");
+    } else {
+      navBar.classList.remove("navbar-sticky-moved-up");
+    }
+    // apply transition
+    if (scroll >= 250) {
+      navBar.classList.add("navbar-sticky-transitioned");
+      scrollTopButton.classList.add("scrollActive");
+    } else {
+      navBar.classList.remove("navbar-sticky-transitioned");
+      scrollTopButton.classList.remove("scrollActive");
+    }
+    // sticky on
+    if (scroll >= 500) {
+      navBar.classList.add("navbar-sticky-on");
+    } else {
+      navBar.classList.remove("navbar-sticky-on");
+    }
+  };
+}
+navMenu();
+//  counter - area*-------------;
+
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    // console.log("couter");
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+          clearInterval(timer);
+        }
+      }, step);
+  }
+
+  counter("count1", 0, 10, 3000);
+  counter("count2", 100, 20, 3500);
+  counter("count3", 0, 40, 3000);
+  counter("count4", 0, 10, 2000);
+});
